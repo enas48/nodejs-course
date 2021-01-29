98e57f6b8e5facaab9b70f1334caa6a124b92de7
enas48 <enasm4829@gmail.com>
2021-01-29 18:48:03 +0200

    update

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const container =require('./container');
+
+container.resolve(function(users){
+    const app = setupExpress();
+
+    function setupExpress(){
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000, ()=>{
+            console.log('listening on port 3000');
+        });
+
+        configureExpress(app);
+
+    // setup router
+    const router = require('express-promise-router')();
+    users.setRouting(router);
+
+    app.use(router);
+
+    }
+
+
+    function configureExpress(app){
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+    }
+
+})
