15e26cc8c7749a4ee02bae6b02ed855c3dd08652
enas48 <enasm4829@gmail.com>
2021-02-01 11:03:52 +0200

    update

--- controllers/users.js
+++ controllers/users.js
@@ -5,11 +5,15 @@ module.exports = function(_){
     return{
         setRouting: function(router){
             router.get('/', this.indexPage);
+            router.get('/signup', this.getSignUp)
 
         
         },
         indexPage : function(req, res){
-            return res.render('index', {test: 'this is test'})
+            return res.render('index')
+        },
+        getSignUp : function(req, res){
+            return res.render('signup')
         }
     }
 }
--- server.js
+++ server.js
@@ -2,16 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
-const container =require('./container');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
+const mongoose=require('mongoose');
+const flash = require('connect-flash');
+const passport =require('passport');
+
+const container = require('./container');
+
 
 container.resolve(function(users){
+    mongoose.Promise = global.Promise;
+    //mongoose.connect('mongodb://localhost/footballkik',{ useNewUrlParser: true })
+
     const app = setupExpress();
 
     function setupExpress(){
         const app = express();
         const server = http.createServer(app);
-        server.listen(3000, ()=>{
-            console.log('listening on port 3000');
+        server.listen(8000, ()=>{
+            console.log('listening on port 8000');
         });
 
         configureExpress(app);
@@ -27,9 +39,22 @@ container.resolve(function(users){
 
     function configureExpress(app){
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+
+       // app.use(validator());
+        app.use(session({
+            secret:'thisisasecretkey',
+            resave: true,
+            saveUninitialized: true,
+            store: new mongoStore({mongooseConnection : mongoose.connection}) 
+        }))
+        app.use(flash())
+
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
 
 })
